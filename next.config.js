97d4760eebd8757859367ec1d0242d204b5563c8
Christoffer Artmann <Artgaard@gmail.com>
2020-08-27 15:46:48 +0200

    fix: Add rewrite for old article URL.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ module.exports = {
       {
         source: '/sitemap.xml',
         destination: '/api/sitemap',
+      },
+      {
+        source: '/articles/waiting-for-network-resources-in-cypress-c9cb5cca-b5d6-49df-bd30-c8c744bad147',
+        destination: '/articles/waiting-for-network-resources-in-cypress'
       }
     ];
   },
