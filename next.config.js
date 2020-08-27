fad59ec726ff921ca6883a4e539d844c6fbebed1
Christoffer Artmann <Artgaard@gmail.com>
2020-08-27 15:49:02 +0200

    fix: Replace rewrite with redirect.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/articles/waiting-for-network-resources-in-cypress-c9cb5cca-b5d6-49df-bd30-c8c744bad147',
+        destination: '/articles/waiting-for-network-resources-in-cypress',
+        permanent: true
+      }
+    ];
+  },
   async rewrites() {
     return [
       {
         source: '/sitemap.xml',
         destination: '/api/sitemap',
-      },
-      {
-        source: '/articles/waiting-for-network-resources-in-cypress-c9cb5cca-b5d6-49df-bd30-c8c744bad147',
-        destination: '/articles/waiting-for-network-resources-in-cypress'
       }
     ];
-  },
+  }
 };
